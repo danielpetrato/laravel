d9b092b038a99e8d3d9561a48ae574a1bd253cdc
Daniel <danielgoomes31@gmail.com>
2019-05-10 15:57:47 -0300

    Criando Rotas

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,32 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cliente',function(){
+    return "HeLLO WORD!!";
+});
+
+// Rota com mais de um nome
+Route::get('/admin/cliente',function(){
+    return "Admin - HeLLO WORD!!";
+});
+
+// Rota com echo
+Route::get('/cliente-echo',function(){
+    echo "Texto com echo";
+});
+
+// Rota com parametro na URL
+Route::get('/produto/{name}',function($name){
+    return "Produto $name";
+});
+
+// Rota com 2 ou mais parametros na URL
+Route::get('/produto/{name}/{id}',function($name,$id){
+    return "Produto $name - $id";
+});
+
+// Rota com 2 ou mais parametros na URL - o id e opcional por causa do ?
+Route::get('/teste/{name}/{id?}',function($name,$id = 1000){
+    return "Produto $name - $id";
+});
