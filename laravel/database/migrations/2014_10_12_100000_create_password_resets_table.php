5f0295bae0d56a426579edb4a615a3211d66f26d
Daniel <danielgoomes31@gmail.com>
2019-05-16 15:46:20 -0300

    Aula 15 - iniciando com laravel- migrations database

--- laravel/database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ laravel/database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Migrations\Migration;
 class CreatePasswordResetsTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations. Aula 15 - iniciando com laravel
      *
      * @return void
      */
